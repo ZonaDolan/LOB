b896443f3e8a4c1aaff5f11d14305319e1f26b8a
sysbreaker <willy_8a_39@yahoo.co.id>
2015-08-24 16:43:33 +0700

    Update Spasi

--- Assets/Script/LaserTrigger.cs
+++ Assets/Script/LaserTrigger.cs
@@ -9,31 +9,31 @@ public class LaserTrigger : MonoBehaviour {
 	public bool trigger;
 	public LineRenderer laser2;
 	
-	void Start(){
+	void Start() {
 		//Inisiasi awal, laser pasti nyala
 		trigger = true;
 	}
 	
 	//Function for Collision Player and Laser
-	void OnTriggerEnter(Collider other){
+	void OnTriggerEnter(Collider other) {
 		GameObject gameObjectCollide = other.gameObject; //buat manggil fungsi death di gameObject Player
 		gameObjectCollide.tag = "Player";
-		if(trigger==true){
-			if(other.gameObject.tag == "Player")
+		if (trigger==true) {
+			if (other.gameObject.tag == "Player")
 			{
 				gameObjectCollide.SendMessage("Death"); //call fucntion Death on Player Script
 				SwitchOff();
 			}
-		}else if(trigger==false){
+		}else if (trigger==false) {
 		}
 	}
 	//Switch On Laser
-	void SwitchOn(){
+	void SwitchOn() {
 		trigger=true;
 		laser2.enabled = true; //line renderer enable
 	}
 	//Switch Off Laser
-	void SwitchOff(){
+	void SwitchOff() {
 		trigger=false;
 		laser2.enabled = false; //line renderer unable
 	}
